2610643b6d903e83fd4021f6c6c189541d12ca85
DESKTOP-3NBEK92\TranVietHieu <hieutran26498@gmail.com>
2021-09-19 15:31:03 +0700

    Add project files.

---
+++ HeThongTracNghiem.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeThongTracNghiem.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
